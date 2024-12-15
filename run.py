2bdf1ae1023e081b5be6af9e8578078eb7bbb56a
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2024-12-15 13:52:10 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1404,7 +1404,7 @@ def mbasic(idf,pwv):
 	nip=random.choice(prox)
 	proxs= {'http': 'socks5://'+nip}
 	ua = random.choice(ugen2)
-	print(ua)
+	print({loop}/{len(ua)})
 	ses = requests.Session()
 	prog.update(des,description=f"[[bold green]Mbasic[bold white]] [[bold green]{idf}[bold white]] {loop}/{len(id)} OK-:[bold green]{ok}[/] CP-:[bold yellow]{cp}[/]")
 	prog.advance(des)
