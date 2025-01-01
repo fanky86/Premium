83bb7f87d93a015331be965d23db8d9d59c30f1d
MR.ExceFaN <106883384+fanky86@users.noreply.github.com>
2025-01-01 16:06:11 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1456,7 +1456,7 @@ def mbasic(idf,pwv):
 	for pw in pwv:
 		try:
 			headers = {
-				'authority': 'mbasic.facebook.com'
+				'authority': 'mbasic.facebook.com',
 				'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
 				'accept-language': 'id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7',
 				'cache-control': 'max-age=0',
