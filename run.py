393fbf91a9b80d403d1214452863dced30e6ae5b
Fanky <106883384+fanky86@users.noreply.github.com>
2025-08-05 02:03:24 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1073,7 +1073,8 @@ def menu():
     elif HaHi in ["3", "03"]:
         prints(Panel(f"""{P2}masukan id target, pastikan id target bersifat publik""",subtitle=f"{P2}ketik {H2}me{P2} untuk dump dari teman sendiri",width=60,style=f"{color_panel}"))
         idfac = console.input(f" {H2}• {P2}Masukan Id Target :{U2} ")
-        dump_id_publik(idfac,"",{"cookie":cookie},token)
+        filename = console.input(f" {H2}• {P2}Masukkan nama file untuk menyimpan hasil dump : ")
+        dump_id_publik(idfac,"",{"cookie":cookie},token,filename)
         print('\n')
         exit()
     elif HaHi in ["4", "04"]:
@@ -1105,30 +1106,24 @@ def dump_publikk(idfac,fields,cookie,token):
     except: pass
 
 #----------[ DUMP-ID-PUBLIK  ]----------#
-def dump_id_publik(idfac_list, fields, cookie, token):
-    if isinstance(idfac_list, str): idfac_list = [idfac_list]
-    id_dump = []
-    headers = {"connection":"keep-alive","accept":"*/*","sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-origin","sec-fetch-user":"?1","sec-ch-ua-mobile":"?1","upgrade-insecure-requests":"1","user-agent":"Mozilla/5.0 (Linux; Android 11; AC2003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537.36","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9"}
-
-    for idfac in idfac_list:
-        after = None
-        while True:
-            try:
-                if after: params = {"access_token":token,"fields":f"name,friends.fields(id,name,birthday).after({after})"}
-                else: params = {"access_token":token,"fields":"name,friends.fields(id,name,birthday)"}
-                url = ses.get(f"https://graph.facebook.com/{idfac}", params=params, headers=headers, cookies=cookie).json()
-                for i in url["friends"]["data"]:
-                    identitas = f'{i["id"]}|{i["name"]}'
-                    if identitas not in id_dump:
-                        id_dump.append(identitas)
-                        sys.stdout.write(f"\r • sedang dump id, berhasil mendapatkan : {len(id_dump)} ID")
-                        sys.stdout.flush()
-                if "next" in url["friends"].get("paging", {}): after = url["friends"]["paging"]["cursors"]["after"]
-                else: break
-            except: break
-
-    print(f"\nSelesai dump total: {len(id_dump)} ID")
-    return id_dump
+def dump_id_publik(idfac, fields, cookie, token, filename):
+    try:
+        headers = {"connection":"keep-alive","accept":"*/*","sec-fetch-dest":"empty","sec-fetch-mode":"cors","sec-fetch-site":"same-origin","sec-fetch-user":"?1","sec-ch-ua-mobile":"?1","upgrade-insecure-requests":"1","user-agent":"Mozilla/5.0 (Linux; Android 11; AC2003) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.104 Mobile Safari/537.36","accept-encoding":"gzip, deflate","accept-language":"id-ID,id;q=0.9"}
+        params = {"access_token":token,"fields":"name,friends.fields(id,name,birthday)"} if len(id)==0 else {"access_token":token,"fields":f"name,friends.fields(id,name,birthday).after({fields})"}
+        url = ses.get(f"https://graph.facebook.com/{idfac}", params=params, headers=headers, cookies=cookie).json()
+        for i in url["friends"]["data"]:
+            identitas = i["id"]+"|"+i["name"]
+            if identitas not in id:
+                id.append(identitas)
+                sys.stdout.write(f"\r • sedang dump id, berhasil mendapatkan : {len(id)} ID")
+                sys.stdout.flush()
+        if "paging" in url["friends"] and "next" in url["friends"]["paging"]:
+            dump_publikk(idfac, url["friends"]["paging"]["cursors"]["after"], cookie, token, filename)
+        if fields == "":
+            with open(filename, "w", encoding="utf-8") as f:
+                for line in id: f.write(line+"\n")
+            print(f"\n[✓] Berhasil disimpan di: {filename}")
+    except: pass
 #----------[ CRACK-massal  ]----------#
 def massal():
     global id
